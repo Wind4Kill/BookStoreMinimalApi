2389fa22d556a43f571032a3dd72142145ace9e4
Artem <artemrom1337@gmail.com>
2026-05-31 22:51:05 +0300

    BookEntityAdded

--- BookStoreMinimalApi.Data/Book.cs
+++ BookStoreMinimalApi.Data/Book.cs
@@ -7,6 +7,13 @@ namespace BookStoreMinimalApi.Data
 {
     public class Book
     {
-        
+        public int BookId { get; set; }
+
+        public string Title { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+
+        public decimal Cost { get; set; }
+
     }
 }
