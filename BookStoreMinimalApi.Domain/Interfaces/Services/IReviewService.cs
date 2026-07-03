27514778a485c404b3ab9ffe19cfa4cf1cca7691
Artem <artemrom1337@gmail.com>
2026-07-03 17:06:35 +0300

    IReviewServiceRefactored

--- BookStoreMinimalApi.Domain/Interfaces/Services/IReviewService.cs
+++ BookStoreMinimalApi.Domain/Interfaces/Services/IReviewService.cs
@@ -2,11 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookStoreMinimalApi.Domain.DTOs;
 
 namespace BookStoreMinimalApi.Domain.Interfaces.Services
 {
     public interface IReviewService
     {
-        Task<int> AddReview();
+        Task<int> AddReview(ReviewDto reviewDto);
     }
 }
